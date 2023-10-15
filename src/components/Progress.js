5eb5ef66ce8528623bebd3634fe5053ad7853eac
Nikhil Mandaliya <nikhilrm8923@gmail.com>
2023-10-15 14:32:16 +0530

    used context and reducer

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,18 +1,17 @@
-export default function Progress({
-    index,
-    numQuestion,
-    points,
-    maxPossiblePoints,
-    answer,
-}) {
+import { useQuiz } from '../context/QuizContext';
+
+export default function Progress() {
+    const { index, numQuestions, points, maxPossiblePoints, answer } =
+        useQuiz();
+
     return (
         <header className="progress">
             <progress
-                max={numQuestion}
+                max={numQuestions}
                 value={index + Number(answer !== null)}
             />
             <p>
-                Question <strong>{index + 1}</strong> / {numQuestion}
+                Question <strong>{index + 1}</strong> / {numQuestions}
             </p>
             <p>
                 <strong>{points}</strong> / {maxPossiblePoints}
